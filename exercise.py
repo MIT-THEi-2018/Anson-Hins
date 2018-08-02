7c6bb2b3be01e4585c1d4353d832123ff551fefb
AChau5212 <170491323@stu.vtc.edu.hk>
2018-08-02 12:39:50 +0800

    heh

---
+++ exercise.py
@@ -0,0 +1,42 @@
+begin = input("Welcome to our board, are you a guest ?\n")
+
+def identity (self):
+    
+    counter = 0
+
+    account = {'Name':[], 'password':[]
+        }
+    
+    
+    def confim (self, name, password):
+        
+        if(begin == no):
+            ac = input("Please enter your user Id & password \n AC:")
+            pw = input("PW:") 
+        
+            if(ac != account["Name"[counter]] and
+            pw != account["password"[counter]]):
+                return confim(counter+1)
+
+            elif (counter  == len(account)):
+                print("Incorrect! Retry!")
+                return identity
+
+        elif(begin == yes):
+            print('Welcome, guest.')
+
+    
+
+
+
+
+
+                
+
+            
+
+
+
+
+
+        
