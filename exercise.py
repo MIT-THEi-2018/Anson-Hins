e22d578a2a9887fa05b2eb23542fc884ebce793b
AChau5212 <170491323@stu.vtc.edu.hk>
2018-08-03 11:03:50 +0800

    yay

--- exercise.py
+++ exercise.py
@@ -1,33 +1,43 @@
-begin = input("Welcome to our board, are you a guest ?\n")
+class request_board:
 
-def identity (self):
-    
-    counter = 0
+    name = []
 
-    account = {'Name':[], 'password':[]
-        }
-    
-    
-    def confim (self, name, password):
+    id = 0
+
+    detail = []
+
+    begin = input("Welcome to our board, are you going to post or search? \n")
+
+    if(begin == "post"):
+
+        post_name = input("Please enter your name \n")
+
+        name.append(post_name)
+
+        request_info =  input("Please enter your request with detail. \n")
+
+        if(request_info is None):
+   
+            print('Invalid input, please retry')
+
+        else:       
+
+            detail.append(request_info)
         
-        if(begin == no):
-            ac = input("Please enter your user Id & password \n AC:")
-            pw = input("PW:") 
-        
-            if(ac != account["Name"[counter]] and
-            pw != account["password"[counter]]):
-                return confim(counter+1)
+            print("Thank you for using our board!")
 
-            elif (counter  == len(account)):
-                print("Incorrect! Retry!")
-                return identity
 
-        elif(begin == yes):
-            print('Welcome, guest.')
+
+
+
+
+
+
+
+       
+
 
     
-
-
 
 
 
